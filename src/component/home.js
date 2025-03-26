e3b64514c09988f5f0f31d3068be698647ab3043
chinnathorn.pro <chinnathorn.pro@cubeofnine.asia>
2025-03-26 12:23:40 +0700

    up

--- src/component/home.js
+++ src/component/home.js
@@ -6,7 +6,7 @@ const Home = () => {
     <Box>
       <div className="container">
         <div className="row">
-          <div className="col-md-5 col-12 d-flex justify-content-center" data-aos="zoom-out">
+          <div className="col-md-5 col-12 d-flex justify-content-center" data-aos="zoom-in">
             <Avatar
               src="https://d3hhrps04devi8.cloudfront.net/bnk48profile/nammonn.jpg"
               className="w-75 w-md-100 h-100"
