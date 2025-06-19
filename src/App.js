7b02ec166260991284cc2f3b36fd811afc1a57ee
CPXDev <cpxdev@outlook.com>
2025-06-19 21:44:20 +0700

    Overlay Start page released

--- src/App.js
+++ src/App.js
@@ -19,10 +19,11 @@ import {
   Snackbar,
   Alert,
   CardHeader,
+  Backdrop,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import Confetti from "react-confetti";
-import CelebrationIcon from '@mui/icons-material/Celebration';
+import CelebrationIcon from "@mui/icons-material/Celebration";
 import SettingsIcon from "@mui/icons-material/Settings";
 import {
   useLocation,
@@ -37,7 +38,7 @@ import Home from "./component/home";
 import NMPlay from "./component/ytplay";
 import Events from "./component/news";
 import Game from "./component/game";
-import OneSignal from 'react-onesignal';
+import OneSignal from "react-onesignal";
 
 import moment, { lang } from "moment";
 
@@ -87,8 +88,9 @@ function App() {
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const [game, setInGame] = React.useState(false);
   const [splash, setSplash] = React.useState(true);
-  const [eight, setEightYear] = React.useState(true);
   const [fire, setFire] = React.useState(false);
+
+  const [overture, setOverTure] = React.useState(false);
 
   const [langcross, setLangCross] = React.useState(
     localStorage.getItem("langconvert") !== null
@@ -127,14 +129,9 @@ function App() {
     document.body.appendChild(addScript);
     window.googleTranslateElementInit = googleTranslateElementInit;
 
-    // OneSignal.init({
-    //   appId: '85a68966-7b47-4ab5-8c66-83042f12289c',
-    //   autoRegister: false,
-    //   notifyButton: {
-    //     enable: false
-    //   },
-    //   allowLocalhostAsSecureOrigin: true
-    // });
+    setTimeout(() => {
+      setOverTure(true);
+    }, 4800);
   }, []);
 
   React.useEffect(() => {
@@ -266,202 +263,276 @@ function App() {
 
   return (
     <Box className="bg-theme">
-      <Confetti
-        numberOfPieces={fire ? 400 : 0}
-        initialVelocityY={200}
-        style={{ position: "fixed" }}
-      />
-      {/* {
-        moment().unix() <= 1748883599 && (
-          <Snackbar anchorOrigin={{ vertical: window.innerWidth > 800 ? 'top' : 'buttom', horizontal: 'center' }} sx={{
-            cursor: 'pointer',
-            width: '97%'
-          }} open={eight} autoHideDuration={1} onClick={() => {
-            if (fire || moment().unix() <= 1748628000 || moment().unix() > 1748883599) {
-              return;
-            }
-            setFire(true)
-            setTimeout(() => {
-              setFire(false)
-            }, 4000);
-          }}>
-            <Alert
-              onClose={() => setEightYear(false)}
-              icon={<CelebrationIcon />}
-              variant="filled"
-              sx={{ background: 'linear-gradient(120deg,rgba(203, 150, 194, 1) 0%, rgba(73, 197, 168, 1) 100%);' }}
-            >
-              <CardHeader title={<h5>8 Years, Eight'finity of Happiness with Nammonn.</h5>} subheader={<small style={{ color: '#333333' }}>มาร่วมเฉลิมฉลองครบรอบ 8 ปีแห่งความสำเร็จของวง BNK48 ไปกับน้ำมนต์กัน{moment().unix() > 1748628000 && moment().unix() <= 1748883599 ? ' แตะหรือคลิกที่การ์ดนี้เลย!' : ''}</small>} />
-            </Alert>
-          </Snackbar>
-        )
-      } */}
-
-      <Slide in={!game} timeout={600}>
-        <AppBar
-          className="newnav"
-          sx={{
-            background: splash ? "transparent !important" : "",
-            borderBottomLeftRadius: 20,
-            borderBottomRightRadius: 20,
-            boxShadow: splash ? "none !important" : "",
-            position: "fixed",
-          }}
-        >
-          <Toolbar>
-            <Box
-              sx={{
-                display: "inline-flex",
-                whiteSpace: "nowrap",
-                flexGrow: 1,
-                padding: 1,
-              }}
+      <Backdrop
+        slots={{ transition: Slide }}
+        open={!overture}
+        timeout={overture ? 1200 : 0}
+        sx={{
+          display: { xs: "none", lg: "flex" },
+          zIndex: 30000,
+          backgroundColor: "#abdef7",
+          borderBottomLeftRadius: overture ? 20 : 0,
+          borderBottomRightRadius: overture ? 20 : 0,
+        }}
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-3 justify-content-end" data-aos="zoom-in">
+              <Avatar
+                alt="pverlayshow"
+                sx={{ width: "100%", height: "100%" }}
+                src={process.env.REACT_APP_OVERLAY}
+              />
+            </div>
+            <div className="col d-flex align-items-center justify-content-center">
+              <div className="text-center">
+                <h1
+                  className="colorpink"
+                  data-aos="zoom-out"
+                  data-aos-duration="12000"
+                >
+                  Welcome to Nammonn BNK48 TH FC
+                </h1>
+                <p
+                  className="colorpink"
+                  data-aos="fade-up"
+                  data-aos-delay="400"
+                >
+                  โลกของน้ำมนต์กำลังจะเริ่มขึ้นแล้ว...
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Backdrop>
+      <Backdrop
+        slots={{ transition: Slide }}
+        open={!overture}
+        timeout={overture ? 1200 : 0}
+        sx={{
+          display: { xs: "flex", lg: "none" },
+          zIndex: 3,
+          backgroundColor: "#abdef7",
+          borderBottomLeftRadius: overture ? 20 : 0,
+          borderBottomRightRadius: overture ? 20 : 0,
+        }}
+      >
+        <div className="container">
+          <div className="row">
+            <div
+              className="col-12 d-flex justify-content-center"
+              data-aos="zoom-in"
             >
               <Avatar
-                onClick={() => his.push("/")}
-                src={process.env.REACT_APP_ICON}
-              />{" "}
-              <Typography
+                alt="pverlayshow"
                 sx={{
-                  fontSize: { xs: 18, sm: 23 },
-                  display: splash
-                    ? { xs: "none !important", md: "block !important" }
-                    : "block",
-                  color: splash ? "#fff" : "#000",
-                  textShadow: splash
-                    ? "0px 0px 40px 70px rgb(0, 0, 0) !important;"
-                    : "",
+                  width: { xs: "50vw", md: "30vw" },
+                  height: { xs: "50vw", md: "30vw" },
                 }}
-                className="d-flex align-items-center link iconweb"
-                translate="no"
-                onClick={() => his.push("/")}
-              >
-                &nbsp;&nbsp;Nammonn BNK48 TH FC
-              </Typography>
-            </Box>
-            <Box sx={{ display: { xs: "none", md: "block" } }}>
-              {navItems.map(
-                (item, i) =>
-                  i > 0 && (
-                    <Button
-                      key={item}
-                      sx={{
-                        color: splash
-                          ? location.pathname === navItemsA[i]
-                            ? "#000"
-                            : "#ededed"
-                          : location.pathname === navItemsA[i]
-                            ? "#fff"
-                            : "#000",
-                        boxShadow: splash
-                          ? "0px 0px 40px 20px rgba(0, 0, 0, 0.13);"
-                          : "",
-                        backgroundColor: splash ? "rgba(0, 0, 0, 0.18)" : "",
-                      }}
-                      onClick={() => his.push(navItemsA[i])}
-                    >
-                      {item}
-                    </Button>
-                  )
-              )}
-              <Button
-                sx={{
-                  color: splash ? "#93e1f5" : "#010e80",
-                  boxShadow: splash
-                    ? "0px 0px 40px 20px rgba(0, 0, 0, 0.13);"
-                    : "",
-                  backgroundColor: splash ? "rgba(0, 0, 0, 0.18)" : "",
-                }}
-                onClick={() =>
-                  window.open(
-                    "https://docs.google.com/forms/d/e/1FAIpQLSffz-2acAHOkZ3uBegcx0SHGYsowCNPFE94CWRvVcVD5F-FFA/viewform?usp=sharing",
-                    "_blank"
-                  )
-                }
-              >
-                Survey
-              </Button>
-              <Button
-                color="inherit"
-                onClick={handleDrawerToggle}
-                sx={{
-                  mr: 1,
-                  display: { xs: "none", md: "initial" },
-                  boxShadow: splash
-                    ? "0px 0px 40px 20px rgba(0, 0, 0, 0.13);"
-                    : "",
-                  backgroundColor: splash ? "rgba(0, 0, 0, 0.18)" : "",
-                  color: splash ? "#fff !important" : "",
-                }}
-              >
-                <SettingsIcon />
-              </Button>
-            </Box>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              edge="start"
-              onClick={handleDrawerToggle}
+                src={process.env.REACT_APP_OVERLAY}
+              />
+            </div>
+            <div className="col mt-5 d-flex align-items-center justify-content-center">
+              <div className="text-center">
+                <h1
+                  className="colorpink"
+                  data-aos="zoom-out"
+                  data-aos-duration="12000"
+                >
+                  Welcome to Nammonn BNK48 TH FC
+                </h1>
+                <p
+                  className="colorpink"
+                  data-aos="fade-up"
+                  data-aos-delay="400"
+                >
+                  โลกของน้ำมนต์กำลังจะเริ่มขึ้นแล้ว...
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Backdrop>
+
+      {overture && (
+        <>
+          <Confetti
+            numberOfPieces={fire ? 400 : 0}
+            initialVelocityY={200}
+            style={{ position: "fixed" }}
+          />
+
+          <Slide in={overture && !game} timeout={600}>
+            <AppBar
+              className="newnav"
               sx={{
-                mr: 1,
-                display: { md: "none" },
-                boxShadow: splash
-                  ? "0px 0px 40px 20px rgba(0, 0, 0, 0.13);"
-                  : "",
-                backgroundColor: splash ? "rgba(0, 0, 0, 0.18)" : "",
-                color: splash ? "#fff !important" : "",
+                background: splash ? "transparent !important" : "",
+                borderBottomLeftRadius: 20,
+                borderBottomRightRadius: 20,
+                boxShadow: splash ? "none !important" : "",
+                position: "fixed",
               }}
             >
-              <MenuIcon />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-      </Slide>
-      <nav>
-        <Drawer
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              borderTopRightRadius: 10,
-              borderBottomRightRadius: 10,
-              backgroundColor: "#ade9f7",
-              width: drawerWidth,
-            },
-          }}
-        >
-          {drawer}
-        </Drawer>
-      </nav>
-      <BasicSwitch>
-        <Route path="/test" render={() => <Home />} />
-        <Route path="/events" render={() => <Events />} />
-        <Route
-          path="/game"
-          render={() => <Game game={game} setInGame={(v) => setInGame(v)} />}
-        />
-        <Route path="/nmplay" render={() => <NMPlay />} />
-        <Route exact render={() => <Home />} />
-      </BasicSwitch>
-      <footer className="card text-center" translate="no">
-        <div className="card-body">
-          <p className="card-title">
-            &copy; Copyright {moment().format("YYYY")} <b>CPXDev</b>, design and
-            maintain for <b>Nammonn BNK48 Thailand Fanclub</b>
-          </p>
-          <small className="card-text">
-            All BNK48 contents are licensed by Independent Artist Management
-            (iAM). These member images and all events poster is objective for
-            Nammonn BNK48 and other BNK48 members supporting only.
-          </small>
-        </div>
-      </footer>
-    </Box >
+              <Toolbar>
+                <Box
+                  sx={{
+                    display: "inline-flex",
+                    whiteSpace: "nowrap",
+                    flexGrow: 1,
+                    padding: 1,
+                  }}
+                >
+                  <Avatar
+                    onClick={() => his.push("/")}
+                    src={process.env.REACT_APP_ICON}
+                  />{" "}
+                  <Typography
+                    sx={{
+                      fontSize: { xs: 18, sm: 23 },
+                      display: splash
+                        ? { xs: "none !important", md: "block !important" }
+                        : "block",
+                      color: splash ? "#fff" : "#000",
+                      textShadow: splash
+                        ? "0px 0px 40px 70px rgb(0, 0, 0) !important;"
+                        : "",
+                    }}
+                    className="d-flex align-items-center link iconweb"
+                    translate="no"
+                    onClick={() => his.push("/")}
+                  >
+                    &nbsp;&nbsp;Nammonn BNK48 TH FC
+                  </Typography>
+                </Box>
+                <Box sx={{ display: { xs: "none", md: "block" } }}>
+                  {navItems.map(
+                    (item, i) =>
+                      i > 0 && (
+                        <Button
+                          key={item}
+                          sx={{
+                            color: splash
+                              ? location.pathname === navItemsA[i]
+                                ? "#000"
+                                : "#ededed"
+                              : location.pathname === navItemsA[i]
+                              ? "#fff"
+                              : "#000",
+                            boxShadow: splash
+                              ? "0px 0px 40px 20px rgba(0, 0, 0, 0.13);"
+                              : "",
+                            backgroundColor: splash
+                              ? "rgba(0, 0, 0, 0.18)"
+                              : "",
+                          }}
+                          onClick={() => his.push(navItemsA[i])}
+                        >
+                          {item}
+                        </Button>
+                      )
+                  )}
+                  <Button
+                    sx={{
+                      color: splash ? "#93e1f5" : "#010e80",
+                      boxShadow: splash
+                        ? "0px 0px 40px 20px rgba(0, 0, 0, 0.13);"
+                        : "",
+                      backgroundColor: splash ? "rgba(0, 0, 0, 0.18)" : "",
+                    }}
+                    onClick={() =>
+                      window.open(
+                        "https://docs.google.com/forms/d/e/1FAIpQLSffz-2acAHOkZ3uBegcx0SHGYsowCNPFE94CWRvVcVD5F-FFA/viewform?usp=sharing",
+                        "_blank"
+                      )
+                    }
+                  >
+                    Survey
+                  </Button>
+                  <Button
+                    color="inherit"
+                    onClick={handleDrawerToggle}
+                    sx={{
+                      mr: 1,
+                      display: { xs: "none", md: "initial" },
+                      boxShadow: splash
+                        ? "0px 0px 40px 20px rgba(0, 0, 0, 0.13);"
+                        : "",
+                      backgroundColor: splash ? "rgba(0, 0, 0, 0.18)" : "",
+                      color: splash ? "#fff !important" : "",
+                    }}
+                  >
+                    <SettingsIcon />
+                  </Button>
+                </Box>
+                <IconButton
+                  color="inherit"
+                  aria-label="open drawer"
+                  edge="start"
+                  onClick={handleDrawerToggle}
+                  sx={{
+                    mr: 1,
+                    display: { md: "none" },
+                    boxShadow: splash
+                      ? "0px 0px 40px 20px rgba(0, 0, 0, 0.13);"
+                      : "",
+                    backgroundColor: splash ? "rgba(0, 0, 0, 0.18)" : "",
+                    color: splash ? "#fff !important" : "",
+                  }}
+                >
+                  <MenuIcon />
+                </IconButton>
+              </Toolbar>
+            </AppBar>
+          </Slide>
+          <nav>
+            <Drawer
+              variant="temporary"
+              open={mobileOpen}
+              onClose={handleDrawerToggle}
+              ModalProps={{
+                keepMounted: true, // Better open performance on mobile.
+              }}
+              sx={{
+                "& .MuiDrawer-paper": {
+                  boxSizing: "border-box",
+                  borderTopRightRadius: 10,
+                  borderBottomRightRadius: 10,
+                  backgroundColor: "#ade9f7",
+                  width: drawerWidth,
+                },
+              }}
+            >
+              {drawer}
+            </Drawer>
+          </nav>
+          <BasicSwitch data-aos="fade-in">
+            <Route path="/test" render={() => <Home />} />
+            <Route path="/events" render={() => <Events />} />
+            <Route
+              path="/game"
+              render={() => (
+                <Game game={game} setInGame={(v) => setInGame(v)} />
+              )}
+            />
+            <Route path="/nmplay" render={() => <NMPlay />} />
+            <Route exact render={() => <Home />} />
+          </BasicSwitch>
+          <footer className="card text-center" translate="no">
+            <div className="card-body">
+              <p className="card-title">
+                &copy; Copyright {moment().format("YYYY")} <b>CPXDev</b>, design
+                and maintain for <b>Nammonn BNK48 Thailand Fanclub</b>
+              </p>
+              <small className="card-text">
+                All BNK48 contents are licensed by Independent Artist Management
+                (iAM). These member images and all events poster is objective
+                for Nammonn BNK48 and other BNK48 members supporting only.
+              </small>
+            </div>
+          </footer>
+        </>
+      )}
+    </Box>
   );
 }
 
