dc1c517a5b24a556dbe46a50897e7ba313d90720
cpxdev <56143786+cpxdevth@users.noreply.github.com>
2025-08-01 08:03:25 +0700

    Update home.js

--- src/component/home.js
+++ src/component/home.js
@@ -85,7 +85,7 @@ function CustomTabPanel(props) {
       aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
-      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+      {value === index && <Box sx={{ p: 1 }}>{children}</Box>}
     </div>
   );
 }
