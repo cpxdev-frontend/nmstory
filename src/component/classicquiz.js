d067aefbb5695db3fb515196b6254a9a9fc87d12
cpxdev <56143786+cpxdevth@users.noreply.github.com>
2025-08-15 13:59:51 +0700

    Update classicquiz.js

--- src/component/classicquiz.js
+++ src/component/classicquiz.js
@@ -161,6 +161,7 @@ const GameApp = ({ game, setInGame, demo }) => {
       timepopupapi();
     } else if (onTimeGameMax - onTimeGame == 60) {
       timepopupapi(true);
+    }
     alert(onTimeGame)
     if (onTimeGameMax - onTimeGame <= 0) {
       clearInterval(gameInterval);
